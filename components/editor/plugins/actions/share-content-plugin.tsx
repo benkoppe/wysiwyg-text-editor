baea1f11718a290aef2bfefb1e4b8860343fc36f
Ben <Koppe.Development@gmail.com>
2025-03-28 18:33:20 -0400

    Format and change caption

--- components/editor/plugins/actions/share-content-plugin.tsx
+++ components/editor/plugins/actions/share-content-plugin.tsx
@@ -1,9 +1,9 @@
 import { useEffect } from "react";
 
 import {
-    SerializedDocument,
-    editorStateFromSerializedDocument,
-    serializedDocumentFromEditorState,
+  SerializedDocument,
+  editorStateFromSerializedDocument,
+  serializedDocumentFromEditorState,
 } from "@lexical/file";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import { CLEAR_HISTORY_COMMAND } from "lexical";
@@ -13,50 +13,48 @@ import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
 
 import {
-    docFromHash,
-    docToHash,
+  docFromHash,
+  docToHash,
 } from "@/components/editor/utils/doc-serialization";
 
 export function ShareContentPlugin() {
-    const [editor] = useLexicalComposerContext();
-    async function shareDoc(doc: SerializedDocument): Promise<void> {
-        const url = new URL(window.location.toString());
-        url.hash = await docToHash(doc);
-        const newUrl = url.toString();
-        window.history.replaceState({}, "", newUrl);
-        await window.navigator.clipboard.writeText(newUrl);
-    }
-    useEffect(() => {
-        docFromHash(window.location.hash).then((doc) => {
-            if (doc && doc.source === "editor") {
-                editor.setEditorState(
-                    editorStateFromSerializedDocument(editor, doc),
-                );
-                editor.dispatchCommand(CLEAR_HISTORY_COMMAND, undefined);
-            }
-        });
-    }, [editor]);
+  const [editor] = useLexicalComposerContext();
+  async function shareDoc(doc: SerializedDocument): Promise<void> {
+    const url = new URL(window.location.toString());
+    url.hash = await docToHash(doc);
+    const newUrl = url.toString();
+    window.history.replaceState({}, "", newUrl);
+    await window.navigator.clipboard.writeText(newUrl);
+  }
+  useEffect(() => {
+    docFromHash(window.location.hash).then((doc) => {
+      if (doc && doc.source === "editor") {
+        editor.setEditorState(editorStateFromSerializedDocument(editor, doc));
+        editor.dispatchCommand(CLEAR_HISTORY_COMMAND, undefined);
+      }
+    });
+  }, [editor]);
 
-    return (
-        <Button
-            variant={"outline"}
-            onClick={() =>
-                shareDoc(
-                    serializedDocumentFromEditorState(editor.getEditorState(), {
-                        source: "editor",
-                    }),
-                ).then(
-                    () => toast.success("URL copied to clipboard"),
-                    () => toast.error("URL could not be copied to clipboard"),
-                )
-            }
-            title="Share"
-            aria-label="Share Playground link to current editor state"
-            size={"sm"}
-            className="p-2"
-        >
-            <SendIcon className="size-4" />
-            Link
-        </Button>
-    );
+  return (
+    <Button
+      variant={"outline"}
+      onClick={() =>
+        shareDoc(
+          serializedDocumentFromEditorState(editor.getEditorState(), {
+            source: "editor",
+          }),
+        ).then(
+          () => toast.success("URL copied to clipboard"),
+          () => toast.error("URL could not be copied to clipboard"),
+        )
+      }
+      title="Share"
+      aria-label="Share link to current editor state"
+      size={"sm"}
+      className="p-2"
+    >
+      <SendIcon className="size-4" />
+      Link
+    </Button>
+  );
 }
